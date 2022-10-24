2a2b653f031a18414c813d07f73da90b5e508a73
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-24 21:56:40 +0300

    add function countdown with control flow

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 // Model
 const stopWatch = {
   timeDisplay: 0,
-  waitingForPause: true,
+  waitingForPause: false,
 };
+
+let interval;
 
 // Controller
 function countdown() {
-  let interval = setInterval(function () {
-    stopWatch.timeDisplay++;
-    if (stopWatch.timeDisplay === 10) {
-      clearInterval(interval);
-    }
-    renderTime();
-  }, 1000);
-};
+  const { waitingForPause } = stopWatch; 
+  if (waitingForPause === false) {
+    stopWatch.waitingForPause = true;
+    interval = setInterval(function() {
+      stopWatch.timeDisplay += 1;
+      renderTime();
+    }, 1000);
+    console.log(stopWatch.waitingForPause)
+  } else if(waitingForPause) {
+    clearInterval(interval)
+    stopWatch.waitingForPause = false;
+    console.log(stopWatch.waitingForPause)
+  }
+}
+console.log(stopWatch.waitingForPause)
 
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", countdown);
@@ -23,6 +32,6 @@ function renderTime() {
   const timeDisplay = document.getElementById("display");
   timeDisplay.value = stopWatch.timeDisplay;
   timeDisplay.style.textAlign = "end";
-};
+}
 
 renderTime();
