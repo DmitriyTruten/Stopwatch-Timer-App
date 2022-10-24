3fc1127fe8609bdfbb130bc0b47ae037649e1fd0
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-24 19:57:02 +0300

    first sketches of MVC

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Model
+const stopWatch = {
+  timeDisplay: 0,
+  waitingForPause: true,
+};
+
+// Controller
+function countdown() {
+  let interval = setInterval(function () {
+    stopWatch.timeDisplay++;
+    if (stopWatch.timeDisplay === 10) {
+      clearInterval(interval);
+    }
+    renderTime();
+  }, 1000);
+};
+
+const startButton = document.getElementById("start");
+startButton.addEventListener("click", countdown());
+
+// View
+function renderTime() {
+  const timeDisplay = document.getElementById("display");
+  timeDisplay.value = stopWatch.timeDisplay;
+  timeDisplay.style.textAlign = "end";
+};
+
+renderTime();
