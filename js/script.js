ceb8fd647040f8c1ff4211d4f90c2217eb88e6e3
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-25 13:36:59 +0300

    handle checking seconds and miliseconds value

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ let interval;
 
 // Controller
 const startButton = document.getElementById("start");
-startButton.addEventListener("click", countdown);
+startButton.addEventListener("click", StartCountdown);
 
 const resetButton = document.getElementById("reset");
 resetButton.addEventListener("click", resetCountdown);
 
-function countdown() {
+function StartCountdown() {
   const { waitingForPause } = stopWatch;
   if (!waitingForPause) {
     stopWatch.waitingForPause = true;
@@ -49,15 +49,28 @@ function resetCountdown() {
   renderTime();
 }
 
+function checkSeconds() {
+  const { seconds } = stopWatch;
+  if (seconds < 10) {
+    return "0" + stopWatch.seconds;
+  } else {
+    return stopWatch.seconds;
+  }
+}
+
+function checkMiliseconds() {
+  const { miliseconds } = stopWatch;
+  if (miliseconds < 10) {
+    return "0" + stopWatch.miliseconds;
+  } else {
+    return stopWatch.miliseconds;
+  }
+}
+
 // View
 function renderTime() {
   const timeDisplay = document.getElementById("display");
-  const { miliseconds } = stopWatch;
-  if(miliseconds < 10) {
-    timeDisplay.value = stopWatch.seconds + "." + "0" + stopWatch.miliseconds;
-  } else {
-    timeDisplay.value = stopWatch.seconds + "." + stopWatch.miliseconds;
-  }
+  timeDisplay.value = checkSeconds() + "." + checkMiliseconds();
   timeDisplay.style.textAlign = "end";
 }
 
