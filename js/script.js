7f6cfd720f717a8b76815934302e451010b68c80
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-02 12:10:40 +0200

    set media query for any width

--- js/script.js
+++ js/script.js
@@ -57,7 +57,7 @@ function StartCountdown() {
       "<img style='margin-left: 0px;' src='images/pause.png'>";
     resetButton.innerHTML =
       "<img style='opacity: 0.5;' src='images/undo-black.png'>";
-    line.style.animation = "circle 60.7s linear infinite";
+    line.style.animation = "circle 60.5s linear infinite";
     handleCountdown("stopWatch");
 
     // If segmentButton is pressed then start the countdown for copied stopwatch object(segmentStopWatch)
