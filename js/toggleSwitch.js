7add376596b1be7c18d0a7a82ede34eff97afb59
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-11 11:04:46 +0200

    refactor toggleSwitch

--- js/toggleSwitch.js
+++ js/toggleSwitch.js
@@ -1,16 +1,16 @@
 const $segmentContainer = $("#segment-container");
 const $segmentDisplay = $("#segment-display");
-const toggleSwitchSlider = document.getElementById("toggle");
+const $toggleSwitchSlider = $("#toggle");
 const $stopWatchDisplay = $("#display");
-const segmentButton = document.getElementById("segment");
-const resetButton = document.getElementById("reset");
-const startButton = document.getElementById("start");
-const lines = document.querySelectorAll(".line");
+const $segmentButton = $("#segment");
+const $resetButton = $("#reset");
+const $startButton = $("#start")
+const lines = document.querySelectorAll('selector');
 
-toggleSwitchSlider.addEventListener("click", toggleSwitch);
+$toggleSwitchSlider.on("click", toggleSwitch);
 
 export function toggleSwitch() {
-  if (toggleSwitchSlider.value === "light") {
+  if ($toggleSwitchSlider.val() === 'light') {
     $(document).ready(() => {
       $(".segment-div").css("border-bottom", "1px solid rgb(255, 255, 255)");
       $("#timer-display").css("color", "white");
@@ -23,35 +23,41 @@ export function toggleSwitch() {
         "<img style='opacity: 0.5;' src='images/bell-white.png'>"
       );
     });
-    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    $(document.body).css("background-color", "rgba(0, 0, 0, 0.8)");
     $stopWatchDisplay.css("background-color", "transparent");
     $segmentDisplay.css("background-color", "transparent");
-    segmentButton.style.backgroundColor = "transparent";
-    resetButton.style.backgroundColor = "transparent";
+    $segmentButton.css("background-color", "transparent");
+    $resetButton.css("background-color", "transparent");
     $segmentContainer.css("color", "white");
     $stopWatchDisplay.css("color", "white");
     $segmentDisplay.css("color", "white");
-    toggleSwitchSlider.value = "dark";
+    $toggleSwitchSlider.val("dark");
     lines.forEach((line) => {
       line.style.backgroundColor = "red";
     });
-    if (startButton.value === "on") {
-      segmentButton.innerHTML =
-        "<img style='opacity: 1;' src='images/stopwatch-white.png'>";
-      resetButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/undo-white.png'>";
-    } else if (startButton.value === "off") {
-      segmentButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/stopwatch-white.png'>";
-      resetButton.innerHTML =
-        "<img style='opacity: 1;' src='images/undo-white.png'>";
+    if ($startButton.val() === "on") {
+      $segmentButton.html(
+        "<img style='opaci;ty: 1;' src='images/stopwatch-white.png'>"
+      );
+      $resetButton.html(
+        "<img style='opacity: 0.5;' src='images/undo-white.png'>"
+      );
+    } else if ($startButton.val() === "off") {
+      $segmentButton.html(
+        "<img style='opacity: 0.5;' src='images/stopwatch-white.png'>"
+      );
+      $resetButton.html(
+        "<img style='opacity: 1;' src='images/undo-white.png'>"
+      );
     } else {
-      segmentButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/stopwatch-white.png'>";
-      resetButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/undo-white.png'>";
+      $segmentButton.html(
+        "<img style='opacity: 0.5;' src='images/stopwatch-white.png'>"
+      );
+      $resetButton.html(
+        "<img style='opacity: 0.5;' src='images/undo-white.png'>"
+      );
     }
-  } else {
+  } else if($toggleSwitchSlider.val() === 'dark') {
     $(document).ready(() => {
       $(".segment-div").css("border-bottom", "1px solid");
       $("#timer-display").css("color", "rgb(84, 84, 84)");
@@ -62,8 +68,8 @@ export function toggleSwitch() {
         "<img style='opacity: 0.5;' src='images/bell-black.png'>"
       );
     });
-    toggleSwitchSlider.value = "light";
-    document.body.style.backgroundColor = "rgba(255, 255, 255)";
+    $toggleSwitchSlider.val("light");
+    $(document.body).css("background-color", "rgb(255, 255, 255)");
     $stopWatchDisplay.css("background-color", "transparent");
     $segmentDisplay.css("background-color", "transparent");
     $stopWatchDisplay.css("color", "rgb(84, 84, 84)");
@@ -72,21 +78,27 @@ export function toggleSwitch() {
     lines.forEach((line) => {
       line.style.backgroundColor = "cyan";
     });
-    if (startButton.value === "on") {
-      segmentButton.innerHTML =
-        "<img style='opacity: 1;' src='images/stopwatch-black.png'>";
-      resetButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/undo-black.png'>";
-    } else if (startButton.value === "off") {
-      segmentButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/stopwatch-black.png'>";
-      resetButton.innerHTML =
-        "<img style='opacity: 1;' src='images/undo-black.png'>";
+    if ($startButton.val() === "on") {
+      $segmentButton.html(
+        "<img style='opacity: 1;' src='images/stopwatch-black.png'>"
+      );
+      $resetButton.html(
+        "<img style='opacity: 0.5;' src='images/undo-black.png'>"
+      );
+    } else if ($startButton.val() === "off") {
+      $segmentButton.html(
+        "<img style='opacity: 0.5;' src='images/stopwatch-black.png'>"
+      );
+      $resetButton.html(
+        "<img style='opacity: 1;' src='images/undo-black.png'>"
+      );
     } else {
-      segmentButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/stopwatch-black.png'>";
-      resetButton.innerHTML =
-        "<img style='opacity: 0.5;' src='images/undo-black.png'>";
+      $segmentButton.html(
+        "<img style='opacity: 0.5;' src='images/stopwatch-black.png'>"
+      );
+      $resetButton.html(
+        "<img style='opacity: 0.5;' src='images/undo-black.png'>"
+      );
     }
   }
 }
