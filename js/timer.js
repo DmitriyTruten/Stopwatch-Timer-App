81465093667c5b31ed573c62901924fc24a1663a
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-13 20:09:23 +0200

    try to scroll for a certain div

--- js/timer.js
+++ js/timer.js
@@ -8,7 +8,7 @@ const timer = {
 };
 
 let previousScrollPosition = 0;
-let NextScrollPosition = 50;
+let nextScrollPosition = 52;
 
 // Controller
 export function timerStyles() {
@@ -24,12 +24,21 @@ export function timerStyles() {
 }
 
 export function numberPicker() {
-  const numbersArray = document.querySelectorAll('#number');
-  numbersArray.forEach(number => {
-    number.addEventListener("scroll", (event) => {
-      number.nextElementSibling.scrollIntoView()
+  const $numbersArray = document.querySelectorAll('#number');
+  $numbersArray.forEach(number => {
+    number.onscroll = (event) => {
+      if(number.scrollTop > previousScrollPosition) {
+        number.scrollTo({top: nextScrollPosition, behavior: "smooth"})
+        previousScrollPosition += 52;
+        nextScrollPosition += 52;
+      }
+      if(number.scrollTop < previousScrollPosition) {
+        number.scrollTo({top: previousScrollPosition, behavior: "smooth"})
+        previousScrollPosition -= 52;
+        nextScrollPosition -= 52;
+      }
       console.log(`previousScrollPosition: ${previousScrollPosition}`)
-      console.log(`NextScrollPosition: ${NextScrollPosition}`)
+      console.log(`NextScrollPosition: ${nextScrollPosition}`)
       if(event.target.matches(".hours")) {
         timer.hours += 1;
       } else if(event.target.matches(".minutes")) {
@@ -38,15 +47,15 @@ export function numberPicker() {
         timer.seconds += 1;
       }
       renderTimerView()
-    })
+    }
   });
 }
 
 export function fillingNumbers() {
   for(let i = 0; i < 60; i++) {
-    $(".hours").append(`<div>${i}</div>`);
-    $(".minutes").append(`<div>${i}</div>`);
-    $(".seconds").append(`<div>${i}</div>`);
+    $(".hours").append(`<input type="text" value=${i} disabled></input>`);
+    $(".minutes").append(`<input type="text" value=${i} disabled></input>`);
+    $(".seconds").append(`<input type="text" value=${i} disabled></input>`);
   }
 }
 
