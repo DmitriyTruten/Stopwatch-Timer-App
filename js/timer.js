c5f75ee2f0b69fa903201abe33c56a63dacd59c2
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-15 11:43:36 +0200

    first steps fo changing number picker logic

--- js/timer.js
+++ js/timer.js
@@ -7,8 +7,7 @@ const timer = {
   countdown: "off",
 };
 
-let previousScrollPosition = 0;
-let nextScrollPosition = 52;
+let numberCounter = 0;
 
 // Controller
 export function timerStyles() {
@@ -24,38 +23,14 @@ export function timerStyles() {
 }
 
 export function numberPicker() {
-  const $numbersArray = document.querySelectorAll('#number');
-  $numbersArray.forEach(number => {
-    number.onscroll = (event) => {
-      if(number.scrollTop > previousScrollPosition) {
-        number.scrollTo({top: nextScrollPosition, behavior: "smooth"})
-        previousScrollPosition += 52;
-        nextScrollPosition += 52;
-      }
-      if(number.scrollTop < previousScrollPosition) {
-        number.scrollTo({top: previousScrollPosition, behavior: "smooth"})
-        previousScrollPosition -= 52;
-        nextScrollPosition -= 52;
-      }
-      console.log(`previousScrollPosition: ${previousScrollPosition}`)
-      console.log(`NextScrollPosition: ${nextScrollPosition}`)
-      if(event.target.matches(".hours")) {
-        timer.hours += 1;
-      } else if(event.target.matches(".minutes")) {
-        timer.minutes += 1;
-      } else {
-        timer.seconds += 1;
-      }
-      renderTimerView()
-    }
-  });
+
 }
 
 export function fillingNumbers() {
   for(let i = 0; i < 60; i++) {
-    $(".hours").append(`<input type="text" value=${i} disabled></input>`);
-    $(".minutes").append(`<input type="text" value=${i} disabled></input>`);
-    $(".seconds").append(`<input type="text" value=${i} disabled></input>`);
+    $(".hours").append(`<input type="text" id=h${i} value=${i} disabled></input>`);
+    $(".minutes").append(`<input type="text" id=${i} value=${i} disabled></input>`);
+    $(".seconds").append(`<input type="text" id=${i} value=${i} disabled></input>`);
   }
 }
 
