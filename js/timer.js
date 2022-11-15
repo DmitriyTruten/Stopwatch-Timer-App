70cfe3d08a9417d69f97164fb69abea0a4410651
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-15 22:05:22 +0200

    add animation on reject, max input value

--- js/timer.js
+++ js/timer.js
@@ -20,38 +20,67 @@ export function timerStyles() {
   });
 }
 
-const inputContainers = document.querySelectorAll('#input-container');
+const inputContainers = document.querySelectorAll("#input-container");
 export function numberPicker() {
-  inputContainers.forEach(container => {
+  inputContainers.forEach((container) => {
     container.addEventListener("keydown", (event) => {
-      if(event.key === "Enter") {
-        scrollIntoNumber(event, container)
+      if (event.key === "Enter") {
+        scrollIntoNumber(event, container);
       }
-    })
-  })
+    });
+  });
 }
 
 function scrollIntoNumber(event, container) {
-  if(event.target === inputContainers[0]) {
+  if (event.target === inputContainers[0]) {
     let inputValue = container.value;
-    const selectedNumber = document.getElementById(`h${inputValue}`);
-    selectedNumber.scrollIntoView({behavior: "smooth", block: "center"})
-  } else if(event.target === inputContainers[1]) {
+    if (inputValue > 59) {
+      rejectInput(container);
+    } else {
+      const selectedNumber = document.getElementById(`h${inputValue}`);
+      selectedNumber.scrollIntoView({ behavior: "smooth", block: "center" });
+      container.value = "";
+    }
+  } else if (event.target === inputContainers[1]) {
     let inputValue = container.value;
-    const selectedNumber = document.getElementById(`m${inputValue}`);
-    selectedNumber.scrollIntoView({behavior: "smooth", block: "center"})
+    if (inputValue > 59) {
+      rejectInput(container);
+    } else {
+      const selectedNumber = document.getElementById(`m${inputValue}`);
+      selectedNumber.scrollIntoView({ behavior: "smooth", block: "center" });
+      container.value = "";
+    }
   } else {
     let inputValue = container.value;
-    const selectedNumber = document.getElementById(`s${inputValue}`);
-    selectedNumber.scrollIntoView({behavior: "smooth", block: "center"})
+    if (inputValue > 59) {
+      rejectInput(container);
+    } else {
+      const selectedNumber = document.getElementById(`s${inputValue}`);
+      selectedNumber.scrollIntoView({ behavior: "smooth", block: "center" });
+      container.value = "";
+    }
   }
 }
 
+function rejectInput(container) {
+  container.value = "";
+  container.style.animation = "reject .5s ease-in-out";
+  setTimeout(() => {
+    container.style.animation = "none";
+  }, 500);
+}
+
 export function fillingNumbers() {
-  for(let i = 0; i < 60; i++) {
-    $(".hours").append(`<input type="text" id=h${i} value=${i} disabled></input>`);
-    $(".minutes").append(`<input type="text" id=m${i} value=${i} disabled></input>`);
-    $(".seconds").append(`<input type="text" id=s${i} value=${i} disabled></input>`);
+  for (let i = 0; i < 60; i++) {
+    $(".hours").append(
+      `<input type="text" id=h${i} value=${i} disabled></input>`
+    );
+    $(".minutes").append(
+      `<input type="text" id=m${i} value=${i} disabled></input>`
+    );
+    $(".seconds").append(
+      `<input type="text" id=s${i} value=${i} disabled></input>`
+    );
   }
 }
 
@@ -60,5 +89,4 @@ export function renderTimerView() {
   $("#timer-display").val(
     "0" + timer.hours + ":" + "0" + timer.minutes + ":" + "0" + timer.seconds
   );
-  
 }
