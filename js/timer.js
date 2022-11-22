9de65f0e13e796b3ec69fa1d1c9ec738714c250f
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-22 18:24:44 +0200

    add setinterval function

--- js/timer.js
+++ js/timer.js
@@ -7,6 +7,8 @@ const timer = {
   countdown: "off",
 };
 
+let timerInterval;
+
 // Controller
 $(".input-container").on("keydown", (event) => {
   if (
@@ -123,18 +125,29 @@ export function outerHandleTimerStart() {
 function innerHandleTimerStart() {
   const { waitingForStart } = timer;
   if (waitingForStart) {
+    timerCountdown()
     timer.waitingForStart = false;
     $("#circle-line").css({
       animation: "reverse-circle 60.5s linear infinite",
     })
     $("#timer-start").html("<img style='margin-left: 0px;' src='images/pause.png'>");
   } else if (!waitingForStart) {
+    clearInterval(timerInterval)
     timer.waitingForStart = true;
     $("#circle-line").css({
       animationPlayState: "paused",
     })
     $("#timer-start").html("<img src='images/play.png'>");
   }
+}
+
+function timerCountdown() {
+  timerInterval = setInterval(() => {
+    timer.seconds--
+    timer.seconds.toString()
+    console.log(timer)
+    renderTimerView()
+  }, 1000)
 }
 
 function formatNumbers() {
@@ -153,11 +166,12 @@ function formatNumbers() {
     return "0" + timer.hours + ":" + timer.minutes + ":" + "0" + timer.seconds;
   } else if (seconds.length === 2) {
     return "0" + timer.hours + ":" + "0" + timer.minutes + ":" + timer.seconds;
+  } else {
+    return (
+      "0" + timer.hours + ":" + "0" + timer.minutes + ":" + "0" + timer.seconds
+    );
   }
 
-  return (
-    "0" + timer.hours + ":" + "0" + timer.minutes + ":" + "0" + timer.seconds
-  );
 }
 
 // View
