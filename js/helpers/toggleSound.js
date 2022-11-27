581cbb7d50b50bbd89d8c4fd04d740c04e35e414
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-27 21:21:11 +0200

    handle error with event.target, add user-select

--- js/helpers/toggleSound.js
+++ js/helpers/toggleSound.js
@@ -1,15 +1,23 @@
 export function toggleSound(event) {
   const { target } = event
   const currentSoundContainer = document.getElementsByClassName("active");
-  currentSoundContainer[0].className =
-    currentSoundContainer[0].className.replace(" active", "");
-
-  if (target.className === "sound") {
-    target.className += " active";
-    target.querySelector("input").checked = true;
-
-  } else if (target.className === "radio") {
-    target.parentNode.className += " active";
-    target.checked = true;
+  try {
+    currentSoundContainer[0].className =
+      currentSoundContainer[0].className.replace(" active", "");
+  
+    if (target.className === "sound") {
+      target.className += " active";
+      target.querySelector("input").checked = true;
+  
+    } else if (target.className === "radio") {
+      target.parentNode.className += " active";
+      target.checked = true;
+    } else if (target.className === 'radio-label') {
+      target.className += " active";
+      target.nextSibling.checked = true;
+    }
+  } catch (error) {
+    return
   }
+
 }
